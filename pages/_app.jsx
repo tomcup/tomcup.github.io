6cffddbde1128e9be1fc56377682adcb60b49f48
tomcup <jiligulu7755@outlook.com>
2022-11-09 23:28:44 +0800

    slove the problem by changing the <image> to <img>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,15 +2,13 @@ import { useEffect } from "react"
 import "bootstrap/dist/css/bootstrap.css"
 
 export default function MyApp({ Component, pageProps }) {
-    useEffect(()=>{
-        typeof document !== undefined ? require("bootstrap") : null;
-    });
+    useEffect(()=>{require("bootstrap/dist/js/bootstrap")}, []);
     return (
         <>
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <image src="/gooddog.png" alt="Tomcup" width="30" height="35" />
+                <a className="navbar-brand" href="/">
+                    <img src="/gooddog.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
                     Navbar
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
