f4029d0a9c07a09bc26982eddc65405c1aa1e376
Tomcup <jiligulu7755@outlook.com>
2022-11-11 11:42:17 +0800

    set _document

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="cn">
+      <Head>
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900&amp;display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
