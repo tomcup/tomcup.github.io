6addc8f5708d6109cf96eb9f69468c2c7cdf00af
Tomcup <jiligulu7755@outlook.com>
2023-01-07 11:10:22 +0800

    clean

--- lib/constants.tsx
+++
@@ -1,4 +0,0 @@
-export const POST_PATH = "_posts";
-export const FEATURES_PATH = "_posts/features";
-
-export const DEBUG = false;
--- lib/debug.tsx
+++
@@ -1,7 +0,0 @@
-import { DEBUG } from "./constants";
-
-export function log(message: Object) {
-  if (DEBUG) {
-    console.log(message);
-  }
-}
